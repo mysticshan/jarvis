ff738f00f5e2a123a5ef7773c0bd9d5176632fa0
mysticshan <mysticshan120@gmail.com>
2026-02-16 23:21:59 +0530

    custumize backend with frontend

--- engine/features.py
+++ engine/features.py
@@ -1,11 +1,13 @@
 import threading
 import time
 import pygame
+import eel 
 
+@eel.expose
 def playAssistantSound():
     def sound():
         music_dir = "www\\assets\\audio\\star_sound.wav"
-        time.sleep(1.5)
+        time.sleep(0.5)
 
         pygame.mixer.init()
         pygame.mixer.music.load(music_dir)
